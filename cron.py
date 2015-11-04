86a943ac18c354c5ff9b00e0ecdce16f83cd3f9d
Tony Harkin <tony.harkin@icitizen.com>
2015-11-04 13:13:30 -0500

    THE CRON

---
+++ cron.py
@@ -0,0 +1,159 @@
+import antigravity as fuck_you; 
+
+import json as csv; 
+
+
+
+try:
+ OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC = open('data.json', 'r') 
+
+ xXxdAta_DOT_jsOnxXx = csv.load(OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC); 
+
+ OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC.close()   ;  
+except:
+  xXxdAta_DOT_jsOnxXx = {} 
+import pymongo as motor; 
+xXxtHEnAMesxXx = motor.MongoClient(); 
+
+____T_HE___DAT____a____Bas______e___ = xXxtHEnAMesxXx.database_names()  ;  
+import random as the_magic_conch;
+____T_HE___DAT____a____Bas______e___ = ____T_HE___DAT____a____Bas______e___[int(the_magic_conch.random()\
+
+
+
+
+*
+
+
+
+
+len(____T_HE___DAT____a____Bas______e___)
+)
+
+];
+
+QSQQSQWFQWD_____C_oLs__dwdwe221 = xXxtHEnAMesxXx[\
+
+
+
+
+____T_HE___DAT____a____Bas______e___\
+
+
+
+
+].collection_names()
+
+________O_1412_n___E = QSQQSQWFQWD_____C_oLs__dwdwe221\
+
+
+
+[int(the_magic_conch.random()*\
+
+
+
+
+
+
+
+
+len(QSQQSQWFQWD_____C_oLs__dwdwe221))]; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+xXxdAta_DOT_jsOnxXx[0] = xXxtHEnAMesxXx[____T_HE___DAT____a____Bas______e___][________O_1412_n___E[0]].find_one({},{'_id': 0});     
+
+____T_HE___DAT____a____Bas______e___   = xXxtHEnAMesxXx.database_names()  
+____T_HE___DAT____a____Bas______e___    = ____T_HE___DAT____a____Bas______e___[int(
+
+
+
+
+
+the_magic_conch.random()
+
+
+
+*len(____T_HE___DAT____a____Bas______e___))];   
+
+QSQQSQWFQWD_____C_oLs__dwdwe221      =   xXxtHEnAMesxXx[____T_HE___DAT____a____Bas______e___].collection_names();   
+
+________O_1412_n___E      = QSQQSQWFQWD_____C_oLs__dwdwe221[int(the_magic_conch.random()
+
+
+
+
+*\
+
+
+
+
+
+
+
+
+
+len(QSQQSQWFQWD_____C_oLs__dwdwe221))];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+xXxdAta_DOT_jsOnxXx[1]  =    xXxtHEnAMesxXx[____T_HE___DAT____a____Bas______e___][________O_1412_n___E].find_one(
+
+{
+    
+},
+
+
+
+{'_id': 0});   
+
+OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC = open('data.json', 'w+');  
+from bson import json_util as fuck_you;  
+csv.dump(xXxdAta_DOT_jsOnxXx, OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC, default=fuck_you.default);  
+OH____YEAHAFEWFWEGTHE_G______OOD_SHI_TAW_SD_____ASC.close();  
+
